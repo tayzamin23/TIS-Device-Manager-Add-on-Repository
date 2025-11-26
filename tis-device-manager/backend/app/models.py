310102648a45206d75308b8a0209c40eea61e218
Tayzar Min <tayzamin.pyay@gmail.com>
2025-11-26 13:25:27 +0630

    Update models.py

--- tis-device-manager/backend/app/models.py
+++ tis-device-manager/backend/app/models.py
@@ -2,22 +2,24 @@ from pydantic import BaseModel
 from typing import List, Optional
 
 class Device(BaseModel):
-ip: str
-port: int = 6000
-name: Optional[str] = None
-subnet_id: Optional[int] = None
-device_id: Optional[int] = None
+    ip: str
+    port: int = 6000
+    name: Optional[str] = None
+    subnet_id: Optional[int] = None
+    device_id: Optional[int] = None
+
 
 class Appliance(BaseModel):
-id: str
-name: str
-device_ip: str
-device_port: int = 6000
-type: str # switch, dimmer, shutter, sensor, thermostat, etc.
-channel: int
-metadata: Optional[dict] = {}
+    id: str
+    name: str
+    device_ip: str
+    device_port: int = 6000
+    type: str  # switch, dimmer, shutter, sensor, etc.
+    channel: int
+    metadata: Optional[dict] = {}
+
 
 class Project(BaseModel):
-name: str
-devices: List[Device] = []
-appliances: List[Appliance] = []
+    name: str
+    devices: List[Device] = []
+    appliances: List[Appliance] = []
