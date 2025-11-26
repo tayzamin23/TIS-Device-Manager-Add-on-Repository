63c9c5308b9ad0a079b7116e829425dcc36446dd
Tayzar Min <tayzamin.pyay@gmail.com>
2025-11-26 16:18:41 +0630

    Create models.py

---
+++ tis-device-manager/app/backend/app/models.py
@@ -0,0 +1,20 @@
+# models.py
+from pydantic import BaseModel
+from typing import List
+
+class Device(BaseModel):
+    name: str
+    ip: str
+    subnet: int
+    device_id: int
+    type: str
+
+class Appliance(BaseModel):
+    name: str
+    type: str
+    device: str
+    channels: dict
+
+class Project(BaseModel):
+    devices: List[Device] = []
+    appliances: List[Appliance] = []
